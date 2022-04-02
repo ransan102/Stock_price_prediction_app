aa964d818818d0de4cb8c81b4e2bf6b7b5994219
harusaku <harusaku0412@yahoo.co.jp>
2022-04-02 20:35:04 +0900

    temp2

---
+++ ml/predict.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from prophet import Prophet
+import pandas_datareader as web
+
+def predict(code, period, column):
+    df = web.DataReader(code, data_source="yahoo", start="2000-01-01")
+    df.reset_index(inplace=True)
+    df_predict = df[["Date", column]].rename(columns={"Date":"ds", column: "y"})
+    
+    m = Prophet()
+    m.fit(df_predict)
+    future = m.make_future_dataframe(periods=365)
+    forecast = m.predict(future)
+    
+    return forecast
+    
